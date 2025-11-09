a4333787fef4a6aa4d8875aa2af232cd3e90a2a4
maxunknowncode <221712067+maxunknowncode@users.noreply.github.com>
2025-11-09 14:03:07 +0100

    feat: add regelwerk embed handler

---
+++ config/regelwerk.js
@@ -0,0 +1,5 @@
+module.exports = {
+  regelwerkCategoryId: '1437045325918634094',
+  regelwerkChannelId: '1437045372089274503',
+  regelwerkMessageId: '', // kann nach dem ersten Senden ergänzt werden
+};
