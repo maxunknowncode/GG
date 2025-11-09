dca378d0a5cfe6398d5918fd6a980bd6b9f48478
maxunknowncode <221712067+maxunknowncode@users.noreply.github.com>
2025-11-09 13:18:38 +0100

    Add automatic guild stats voice channel updater

--- index.js
+++ index.js
@@ -1,15 +1,19 @@
 const { Client, GatewayIntentBits } = require('discord.js');
+const { startStatsUpdater } = require('./tasks/updateStats');
 
 const client = new Client({
   intents: [
     GatewayIntentBits.Guilds,
     GatewayIntentBits.GuildMessages,
     GatewayIntentBits.MessageContent,
+    GatewayIntentBits.GuildMembers,
+    GatewayIntentBits.GuildPresences,
   ],
 });
 
 client.once('ready', () => {
   console.log(`Eingeloggt als ${client.user.tag}`);
+  startStatsUpdater(client);
 });
 
 client.login(process.env.TOKEN).catch((error) => {
